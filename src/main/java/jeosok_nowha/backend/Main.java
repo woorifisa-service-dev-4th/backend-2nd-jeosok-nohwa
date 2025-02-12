2a2f6c19c188f1552c41982ba3e1c3b4a7f19780
HyeYoung-Hwang <vkfvks1004@naver.com>
2025-02-12 16:53:32 +0900

    feat:ã출력 파 util 생성

--- src/main/java/jeosok_nowha/backend/Main.java
+++ src/main/java/jeosok_nowha/backend/Main.java
@@ -1,7 +1,10 @@
 package jeosok_nowha.backend;
 
+import jeosok_nowha.backend.utils.PrintUtils;
+
 public class Main {
 	public static void main(String[] args) {
+
 		System.out.println("Hello, World!");
 	}
 }
--- src/main/java/jeosok_nowha/backend/domain/score/controller/ScoreController.java
+++ src/main/java/jeosok_nowha/backend/domain/score/controller/ScoreController.java
@@ -1,5 +1,12 @@
 package jeosok_nowha.backend.domain.score.controller;
 
+import jeosok_nowha.backend.utils.PrintUtils;
+
 public class ScoreController {
 
+
+
+	public void getRanking(){
+
+	}
 }
