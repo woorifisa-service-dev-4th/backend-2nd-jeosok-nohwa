3fd85d9aae7679dc3837392b67d878c5267733e7
taegung <yesongmin12@gamil.com>
2025-02-14 12:21:26 +0900

    feat : MemberControllerTest 코드 작성

---
+++ src/test/java/jeosok_nowha/backend/domain/member/controller/MemberControllerTest.java
@@ -0,0 +1,20 @@
+package jeosok_nowha.backend.domain.member.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MemberControllerTest {
+
+	@Test
+	void run() {
+	}
+
+	@Test
+	void signUp() {
+	}
+
+	@Test
+	void signIn() {
+	}
+}
